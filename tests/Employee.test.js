555e338245dbc3b44bf05bf28d20bd7a9b667bcc
areye022 <areye022@gmail.com>
2020-10-15 21:33:43 -0700

    adjusted spacing

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,26 +1,26 @@
 const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
-  const e = new Employee();
-  expect(typeof(e)).toBe("object");
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
-  const name = "Alice";
-  const e = new Employee(name);
-  expect(e.name).toBe(name);
+    const name = "Alice";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
-  const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue);
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
-  const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
-  expect(e.email).toBe(testValue);
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
